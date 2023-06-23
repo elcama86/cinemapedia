ac93b65264dc6c4c3c56d98929f81ca376d33b3d
Hector Montilla <elcama_86@msn.com>
2023-06-23 11:20:11 -0400

    se agrega conservacion de estado con go_router

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,37 +1,55 @@
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:cinemapedia/presentation/screens/screens.dart';
 import 'package:cinemapedia/presentation/views/views.dart';
 
+final _rootNavigatorKey = GlobalKey<NavigatorState>();
+final _shellNavigatorMovieKey =
+    GlobalKey<NavigatorState>(debugLabel: 'shellMovie');
+final _shellNavigatorFavoritesKey =
+    GlobalKey<NavigatorState>(debugLabel: 'shellFavorites');
+
 final appRouter = GoRouter(
   initialLocation: "/",
+  navigatorKey: _rootNavigatorKey,
   routes: [
-    ShellRoute(
-      builder: (context, state, child) {
+    StatefulShellRoute.indexedStack(
+      builder: (context, state, navigationShell) {
         return HomeScreen(
-          childView: child,
+          navigationShell: navigationShell,
         );
       },
-      routes: [
-        GoRoute(
-          path: '/',
-          builder: (context, state) => const HomeView(),
+      branches: [
+        StatefulShellBranch(
+          navigatorKey: _shellNavigatorMovieKey,
           routes: [
             GoRoute(
-              path: 'movie/:id',
-              name: MovieScreen.name,
-              builder: (context, state) {
-                final movieId = state.pathParameters['id'] ?? 'no-id';
+              path: '/',
+              builder: (context, state) => const HomeView(),
+              routes: [
+                GoRoute(
+                  path: 'movie/:id',
+                  name: MovieScreen.name,
+                  builder: (context, state) {
+                    final movieId = state.pathParameters['id'] ?? 'no-id';
 
-                return MovieScreen(
-                  movieId: movieId,
-                );
-              },
+                    return MovieScreen(
+                      movieId: movieId,
+                    );
+                  },
+                ),
+              ],
             ),
           ],
         ),
-        GoRoute(
-          path: '/favorites',
-          builder: (context, state) => const FavoritesView(),
+        StatefulShellBranch(
+          navigatorKey: _shellNavigatorFavoritesKey,
+          routes: [
+            GoRoute(
+              path: '/favorites',
+              builder: (context, state) => const FavoritesView(),
+            ),
+          ],
         ),
       ],
     ),
--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,20 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:cinemapedia/presentation/widgets/widgets.dart';
+import 'package:go_router/go_router.dart';
 
 class HomeScreen extends StatelessWidget {
   static const String name = "home-screen";
 
-  final Widget childView;
+  final StatefulNavigationShell navigationShell;
 
   const HomeScreen({
     super.key,
-    required this.childView,
+    required this.navigationShell,
   });
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: childView,
+      body: navigationShell,
       bottomNavigationBar: const CustomBottomNavigation(),
     );
   }
