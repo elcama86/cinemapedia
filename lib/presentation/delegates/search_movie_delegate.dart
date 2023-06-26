f1772a1eb2b6a260251073282ccc3ff6d02486d2
Hector Montilla <elcama_86@msn.com>
2023-06-26 16:51:56 -0400

    movie item changed in search movie delegate

--- lib/presentation/delegates/search_movie_delegate.dart
+++ lib/presentation/delegates/search_movie_delegate.dart
@@ -138,75 +138,78 @@ class _MovieItem extends StatelessWidget {
       onTap: () {
         onMovieSelected(context, movie);
       },
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
-        child: Row(
-          children: [
-            // Imagen de película
-            SizedBox(
-              width: size.width * 0.2,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(10.0),
-                child: Image.network(
-                  movie.posterPath,
-                  loadingBuilder: (context, child, loadingProgress) {
-                    return FadeIn(child: child);
-                  },
+      child: FadeIn(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
+          child: Row(
+            children: [
+              // Imagen de película
+              SizedBox(
+                width: size.width * 0.2,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10.0),
+                  child: FadeInImage(
+                    height: 130,
+                    fit: BoxFit.cover,
+                    image: NetworkImage(movie.posterPath),
+                    placeholder:
+                        const AssetImage('assets/loaders/bottle-loader.gif'),
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(
-              width: 10.0,
-            ),
-            // Descripción
-            SizedBox(
-              width: size.width * 0.7,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  RichText(
-                    textAlign: TextAlign.justify,
-                    text: TextSpan(
-                      style: textStyles.titleSmall,
-                      children: [
-                        TextSpan(
-                          text: movie.title,
-                          style: const TextStyle(
-                            fontWeight: FontWeight.bold,
+              const SizedBox(
+                width: 10.0,
+              ),
+              // Descripción
+              SizedBox(
+                width: size.width * 0.7,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    RichText(
+                      textAlign: TextAlign.justify,
+                      text: TextSpan(
+                        style: textStyles.titleSmall,
+                        children: [
+                          TextSpan(
+                            text: movie.title,
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
+                          TextSpan(
+                            text: " (${HumanFormats.year(movie.releaseDate)})",
+                          ),
+                        ],
+                      ),
+                    ),
+                    Text(
+                      (movie.overview.length > 100)
+                          ? "${movie.overview.substring(0, 100)}..."
+                          : movie.overview,
+                      textAlign: TextAlign.justify,
+                    ),
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.star_half_rounded,
+                          color: Colors.yellow.shade800,
                         ),
-                        TextSpan(
-                          text: " (${HumanFormats.year(movie.releaseDate)})",
+                        const SizedBox(
+                          width: 5.0,
+                        ),
+                        Text(
+                          HumanFormats.number(movie.voteAverage, 1),
+                          style: textStyles.bodyMedium!
+                              .copyWith(color: Colors.yellow.shade900),
                         ),
                       ],
                     ),
-                  ),
-                  Text(
-                    (movie.overview.length > 100)
-                        ? "${movie.overview.substring(0, 100)}..."
-                        : movie.overview,
-                    textAlign: TextAlign.justify,
-                  ),
-                  Row(
-                    children: [
-                      Icon(
-                        Icons.star_half_rounded,
-                        color: Colors.yellow.shade800,
-                      ),
-                      const SizedBox(
-                        width: 5.0,
-                      ),
-                      Text(
-                        HumanFormats.number(movie.voteAverage, 1),
-                        style: textStyles.bodyMedium!
-                            .copyWith(color: Colors.yellow.shade900),
-                      ),
-                    ],
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
