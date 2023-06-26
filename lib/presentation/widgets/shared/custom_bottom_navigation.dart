0eda2479aa3eba4f42f7152a0dfd8927978f5d42
Hector Montilla <elcama_86@msn.com>
2023-06-26 14:32:50 -0400

    cambio de categorias por populares en nav bar

--- lib/presentation/widgets/shared/custom_bottom_navigation.dart
+++ lib/presentation/widgets/shared/custom_bottom_navigation.dart
@@ -12,18 +12,21 @@ class CustomBottomNavigation extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
     return BottomNavigationBar(
       elevation: 0.0,
       currentIndex: currentIndex,
       onTap: (index) => onItemTapped(index),
+      selectedItemColor: colors.primary,
       items: const [
         BottomNavigationBarItem(
           icon: Icon(Icons.home_max),
           label: "Inicio",
         ),
         BottomNavigationBarItem(
-          icon: Icon(Icons.label_outline),
-          label: "Categorías",
+          icon: Icon(Icons.thumbs_up_down_outlined),
+          label: "Populares",
         ),
         BottomNavigationBarItem(
           icon: Icon(Icons.favorite_outline),
